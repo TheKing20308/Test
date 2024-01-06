ef4e53528f9427d3fcc6a551516f623a12cae2d0
TheKing20308 <palprothom21@gmail.com>
2024-01-06 15:45:07 +0530

    Ui manager and Leentween

---
+++ Assets/Game/Scripts/Api/SignUp.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Game.Scripts.Api
+{
+    public class SignUp: MonoBehaviour
+    {
+        
+    }
+
+    public class Request
+    {
+        public const string name = "";
+        public const string email = "";
+        public const string password = "";
+    }
+
+    public class Response
+    {
+        
+    }
+}
